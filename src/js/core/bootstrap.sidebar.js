e1fc55fae9d2a827da2150ad6d258eb2defe93ab
Doug Hill <quickstep25@me.com>
2015-04-08 01:13:25 -0400

    updated sidebar toggle button

--- src/js/core/bootstrap.sidebar.js
+++ src/js/core/bootstrap.sidebar.js
@@ -1,32 +1,13 @@
-/*global console,jQuery*/
+/*global console, jQuery*/
 jQuery(document).ready(function ($) {
 	"use strict";
-	
-	var $width = $(document).width(),
-		$sidebar = $('#wrapper-sidebar'),
-		$core	= $('#wrapper-corecontent');
-			
-	    
-	if ($width > 992) {
-		console.log('Window size is greater than mobile device. Setting sidebar menu opened on initial state.');
+    $(document).on('click', '#sidebar-toggle', null, function () {
+		var $sidebar	= $('#container-sidebar'),
+			$content	= $('#wrapper-content'),
+			$button		= $('#sidebar-toggle');
 
-		$('#wrapper-sidebar').addClass('open');
-		$('#wrapper-corecontent').addClass('open');
-		$('#sidebar-toggle').toggleClass('active');
-	}
-    
-    $('#sidebar-toggle').click(function () {
-	    var $element = $(this);
-		
-		if (!$sidebar.hasClass('animate')) {
-			$sidebar.addClass('animate');
-		}
-		if (!$core.hasClass('animate')) {
-			$core.addClass('animate');
-		}
-		
-		$(this).toggleClass('active');
-        $('#wrapper-sidebar').toggleClass('open');
-        $('#wrapper-corecontent').toggleClass('open');
+		$button.toggleClass('active');
+        $sidebar.toggleClass('sidebar-closed');
+        $content.toggleClass('sidebar-closed');
     });
 });
